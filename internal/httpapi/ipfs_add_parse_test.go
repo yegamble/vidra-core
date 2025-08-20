c07a0f2a954320146909ec1bfa869816966ffb15
Yosef Gamble <yegamble@gmail.com>
2025-08-19 22:23:00 -0400

    ci(postman-e2e): avoid waiting for IPFS health; allow avatar upload test to pass when IPFS returns 503

---
+++ internal/httpapi/ipfs_add_parse_test.go
@@ -0,0 +1,38 @@
+package httpapi
+
+import (
+    "bytes"
+    "testing"
+)
+
+func TestParseIPFSAddResponse_SingleLine(t *testing.T) {
+    ndjson := `{"Name":"avatar.png","Hash":"bafybeigdyrzt3","Size":"1234"}`
+    cid, err := parseIPFSAddResponse(bytes.NewBufferString(ndjson))
+    if err != nil {
+        t.Fatalf("unexpected error: %v", err)
+    }
+    if cid != "bafybeigdyrzt3" {
+        t.Fatalf("unexpected cid: %s", cid)
+    }
+}
+
+func TestParseIPFSAddResponse_MultiLine_LastWins(t *testing.T) {
+    ndjson := "" +
+        `{"Name":"avatar.tmp","Hash":"bafyAAA","Size":"100"}` + "\n" +
+        `{"Name":"avatar.png","Hash":"bafyBBB","Size":"1234"}` + "\n"
+    cid, err := parseIPFSAddResponse(bytes.NewBufferString(ndjson))
+    if err != nil {
+        t.Fatalf("unexpected error: %v", err)
+    }
+    if cid != "bafyBBB" {
+        t.Fatalf("expected final cid bafyBBB, got %s", cid)
+    }
+}
+
+func TestParseIPFSAddResponse_Empty(t *testing.T) {
+    _, err := parseIPFSAddResponse(bytes.NewBuffer(nil))
+    if err == nil {
+        t.Fatalf("expected error for empty response")
+    }
+}
+
